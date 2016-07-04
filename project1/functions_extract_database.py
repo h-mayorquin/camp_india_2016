1488a35a667122e2e8c059bb02a06a87430a49d6
heberto <h.mayorquin@gmail.com>
2016-07-04 14:23:20 +0530

    Extract views

--- project1/functions_extract_database.py
+++ project1/functions_extract_database.py
@@ -1,4 +1,6 @@
-def view_trial(trial):
+from __future__ import print_function
+
+def view_trial(trials, trial_N):
     """
     Prints all the details from a given trial
     :param trial: a trial from the database
@@ -7,6 +9,10 @@ def view_trial(trial):
     type_of_data_to_index = {'Time of Cue': 0, 'Time of Sample': 1, 'trialnum': 2, 'spiketrain': 3, 'IsMatch': 4,
                              'fix': 5,
                              'cuerate': 6, 'cuedelay:': 7, 'samplerate': 8, 'sampledelay': 9, 'Time of Reward': 10}
+
+    print('---------------------------------')
+    print('Trial Number', trial_N)
+    trial = trials[trial_N]
 
     for key, value in type_of_data_to_index.items():
         if key not in ['spiketrain']:
