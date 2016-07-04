37d46b7608789a2913e85602b4bcef0540b07ef3
heberto <h.mayorquin@gmail.com>
2016-07-04 14:22:32 +0530

    Modify load data

---
+++ project1/functions_extract_database.py
@@ -0,0 +1,16 @@
+def view_trial(trial):
+    """
+    Prints all the details from a given trial
+    :param trial: a trial from the database
+    :return: None
+    """
+    type_of_data_to_index = {'Time of Cue': 0, 'Time of Sample': 1, 'trialnum': 2, 'spiketrain': 3, 'IsMatch': 4,
+                             'fix': 5,
+                             'cuerate': 6, 'cuedelay:': 7, 'samplerate': 8, 'sampledelay': 9, 'Time of Reward': 10}
+
+    for key, value in type_of_data_to_index.items():
+        if key not in ['spiketrain']:
+            print(key, trial[value][0][0])
+        else:
+            print(key, trial[value])
+
--- project1/load_data.py
+++ project1/load_data.py
@@ -1,10 +1,37 @@
+from __future__ import print_function
 import scipy.io as sio
 
-folder = "./database/"
-data_base_name = 'SummaryDatabase.mat'
-data_base = sio.loadmat(folder + data_base_name)
+from functions_extract_database import view_trial
 
-pre_spatial_set = data_base['Pre_SpatialSet']
-pre_feature_sets = data_base['Pre_FeatureSets']
-spike_train_SpatialSet = data_base['SPtrain_SpatialSet']
 
+folder = "./data/"
+data_base_name = 'ADR001_1_3000'
+format = '.mat'
+data_base = sio.loadmat(folder + data_base_name + format)
+
+monkeys = {1: 'ADR', 2: 'BEN', 3: 'ELV', 4: 'SCR'}
+
+data = data_base['MatData']
+
+# 8 or 9 blocks of 12 trials each
+blocks = data[0][0][0]
+print('This should be 8 or 9', blocks.shape)
+
+# Which of all the blocks you chose
+block_N = 6
+
+trials = blocks[:, block_N][0][0][0]
+print('This should be 12', trials.shape)
+
+# Which of the trials you chose
+trial_N = 4
+trial = trials[trial_N]
+
+code = [('Cue_onT', 'O'), ('Sample_onT', 'O'), ('trialnum', 'O'), ('TS', 'O'), ('IsMatch', 'O'), ('fix', 'O'), ('cuerate', 'O'),
+ ('cuedelay', 'O'), ('samplerate', 'O'), ('sampledelay', 'O'), ('Reward_onT', 'O')]
+
+type_of_data_to_index = {'Time of Cue': 0, 'Time of Sample': 1, 'trialnum': 2, 'spiketrain': 3, 'IsMatch': 4, 'fix': 5,
+                         'cuerate': 6, 'cuedelay:': 7, 'samplerate': 8, 'sampledelay': 9, 'Time of Reward': 10}
+
+
+view_trial(trials, trial_N)
