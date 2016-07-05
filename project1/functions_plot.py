212a47dbaf88fec82128db1c8c30590ee8d1d369
heberto <h.mayorquin@gmail.com>
2016-07-05 21:27:56 +0530

    Added a function that plots the grid of raster plot and the psth

--- project1/functions_plot.py
+++ project1/functions_plot.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 from functions_extract_database import get_spike_train_from_trial
 import seaborn as sns
 
 sns.set(font_scale=2.0)
+sns.set_style('white')
 
 
 def plot_raster_from_trials(trials, ax=None):
@@ -31,3 +33,48 @@ def plot_raster_from_trials(trials, ax=None):
     ax.set_ylim([0, numbers_of_trials + 1])
 
     return ax
+
+
+def plot_spike_raster_grid(blocks, block_to_grid_map):
+    fig = plt.figure(figsize=(16, 12))
+    number_of_blocks = blocks.size
+    gs = gridspec.GridSpec(3, 3)
+    for block_N in range(number_of_blocks):
+        trials = blocks[:, block_N][0][0][0]
+        x, y = block_to_grid_map[block_N + 1]
+        ax = fig.add_subplot(gs[x, y])
+        ax = plot_raster_from_trials(trials, ax)
+
+    return fig
+
+
+def plot_psth_from_trials(trials, ax=None):
+    """
+    This function plots the peri-stimulus time histogram from a
+    set of trials of spike trains
+    :param trials: a collection of trials
+    :param ax: an axis of matplotlib
+
+    :return: the axis (matplotlib)
+    """
+
+    if ax is None:
+        fig = plt.figure(figsize=(16, 12))
+        fig.hold(True)
+        ax = fig.add_subplot(111)
+
+    numbers_of_trials = trials.size
+    all_spikes = []
+    # We need to plot the raster plot
+    for trial_N in range(numbers_of_trials):
+        trial = trials[trial_N]
+        spikes = get_spike_train_from_trial(trial)
+        all_spikes = np.concatenate((all_spikes, spikes))
+
+    histogram_data = ax.hist(all_spikes, bins=10)
+
+    ax.set_xlabel('Time (ms)')
+
+    return ax, histogram_data
+
+def plot_psth_grid(blocks block_to_grid_map):
--- project1/load_data.py
+++ project1/load_data.py
@@ -2,10 +2,12 @@ from __future__ import print_function
 import scipy.io as sio
 
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
 import numpy as np
 
-from functions_plot import plot_raster_from_trials
-
+from functions_plot import plot_raster_from_trials, plot_spike_raster_grid
+from functions_plot import plot_raster_from_trials, plot_psth_from_trials
 from functions_extract_database import view_trial, get_spike_train_from_trial
 
 
@@ -20,19 +22,29 @@ data = data_base['MatData']
 
 # 8 or 9 blocks of 12 trials each
 blocks = data[0][0][0]
-print('This should be 8 or 9', blocks.shape)
-number_of_blocks = 
+number_of_blocks = blocks.size
+print('This should be 8 or 9', number_of_blocks)
+
 # Which of all the blocks you chose
 block_N = 6
 
 trials = blocks[:, block_N][0][0][0]
-numbers_of_trials = trials.size
+number_of_trials = trials.size
+# number_of_trials = trials.size
 
-print('This should be 12', numbers_of_trials)
+print('This should be 12', number_of_trials)
+
+block_to_grid_map = {1:(1, 2), 2: (0, 2), 3:(0, 1), 4: (0, 0), 5: (1, 0),
+                     6: (2, 0), 7: (2, 1), 8: (2, 2), 9: (1, 1)}
 
 
-ax = plot_raster_from_trials(trials)
+
+ax, data = plot_psth_from_trials(trials)
+print(data[0])
+print(data[0].size)
 plt.show()
+# fig = plot_spike_raster_grid(blocks, block_to_grid_map)
+# plt.show(fig)
 
 
 
